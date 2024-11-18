9a6aec4bd39f4aa85f0aedebd7e46346a0a66d73
Edwin Ong <edwin@Edwins-MacBook-Air.local>
2024-11-18 00:54:49 -0800

    changed port

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/process', methods=['POST'])
+@app.route('/process', methods=['POST','GET'])
 def process_twostem():
+    if request.method == 'GET':
+        return "Hello, World!"
     print(f"Request headers: {request.headers}")
     if 'file' not in request.files:
         return jsonify({"error": "No file uploaded"}), 400
